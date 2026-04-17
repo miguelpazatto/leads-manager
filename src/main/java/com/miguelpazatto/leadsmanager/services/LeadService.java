2de6cf8d2457e76021cb301e905a24a6dbb7d666
Miguel Pazatto <miguelpazatto60@gmail.com>
2026-04-17 08:38:15 -0300

    Implementing method findById on Lead entity

--- src/main/java/com/miguelpazatto/leadsmanager/services/LeadService.java
+++ src/main/java/com/miguelpazatto/leadsmanager/services/LeadService.java
@@ -1,6 +1,7 @@
 package com.miguelpazatto.leadsmanager.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,10 @@ public class LeadService {
 	
 	public List<Lead> findAll() {
 		return repository.findAll();
+	}
+	
+	public Lead findById(Long id) {
+		Optional<Lead> obj = repository.findById(id);
+		return obj.orElseThrow();
 	}
 }
