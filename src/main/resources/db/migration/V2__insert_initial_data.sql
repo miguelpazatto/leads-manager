a96d7813ff5e9d21df51a8519b5def6456fe3320
Miguel Pazatto <miguelpazatto60@gmail.com>
2026-05-05 13:31:17 -0300

    fixing SQL queries on migration

--- src/main/resources/db/migration/V2__insert_initial_data.sql
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -18,38 +18,38 @@ VALUES (1, 'Sinto cansaço apenas em dias de carga de trabalho excessiva.', 15, 
 INSERT INTO tb_option (id, description, weight, question_id) 
 VALUES (1, 'Mantenho disposição alta e cansaço leve apenas ao fim do dia.', 25, 1);
 
-INSERT INTO tb_question (id, statement) VALUES (1, 'Como você descrevia a eficiência do seu planejamento diário?');
+INSERT INTO tb_question (id, statement) VALUES (2, 'Como você descrevia a eficiência do seu planejamento diário?');
 
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Passo o dia resolvendo urgências e não foco no que é estratégico.', 4, 1);
+VALUES (1, 'Passo o dia resolvendo urgências e não foco no que é estratégico.', 4, 2);
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Consigo planejar meu dia, mas as interrupções são constantes.', 8, 1);
+VALUES (1, 'Consigo planejar meu dia, mas as interrupções são constantes.', 8, 2);
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Tenho uma rotina organizada, com poucas falhas de cronograma.', 15, 1);
+VALUES (1, 'Tenho uma rotina organizada, com poucas falhas de cronograma.', 15, 2);
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Minha agenda é totalmente controlada e delegada com eficiência.', 25, 1);
+VALUES (1, 'Minha agenda é totalmente controlada e delegada com eficiência.', 25, 2);
 
-INSERT INTO tb_question (id, statement) VALUES (1, 'Como você descrevia a seu foco e raciocínio com o passar do dia?');
-
-INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Sinto névoa mental e dificuldade constante em tomar decisões simples.', 4, 1);
-INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Consigo focar, mas me distraio facilmente com e-mails e celular.', 8, 1);
-INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Tenho blocos de foco profundo, mas oscilo em dias estressantes.', 15, 1);
-INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Mantenho clareza total e foco absoluto nas metas principais.', 25, 1);
-
-INSERT INTO tb_question (id, statement) VALUES (1, 'Como você descrevia seu autocuidado e tempo fora do trabalho?');
+INSERT INTO tb_question (id, statement) VALUES (3, 'Como você descrevia a seu foco e raciocínio com o passar do dia?');
 
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'O trabalho consome minhas noites e fins de semana; sem tempo para família/saúde.', 4, 1);
+VALUES (1, 'Sinto névoa mental e dificuldade constante em tomar decisões simples.', 4, 3);
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Tento me desligar, mas estou sempre checando notificações do trabalho.', 8, 1);
+VALUES (1, 'Consigo focar, mas me distraio facilmente com e-mails e celular.', 8, 3);
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Consigo separar os horários, embora o estresse ocasionalmente interfira.', 15, 1);
+VALUES (1, 'Tenho blocos de foco profundo, mas oscilo em dias estressantes.', 15, 3);
 INSERT INTO tb_option (id, description, weight, question_id) 
-VALUES (1, 'Tenho equilíbrio total e tempo dedicado para lazer e autocuidado.', 25, 1);
+VALUES (1, 'Mantenho clareza total e foco absoluto nas metas principais.', 25, 3);
+
+INSERT INTO tb_question (id, statement) VALUES (4, 'Como você descrevia seu autocuidado e tempo fora do trabalho?');
+
+INSERT INTO tb_option (id, description, weight, question_id) 
+VALUES (1, 'O trabalho consome minhas noites e fins de semana; sem tempo para família/saúde.', 4, 4);
+INSERT INTO tb_option (id, description, weight, question_id) 
+VALUES (1, 'Tento me desligar, mas estou sempre checando notificações do trabalho.', 8, 4);
+INSERT INTO tb_option (id, description, weight, question_id) 
+VALUES (1, 'Consigo separar os horários, embora o estresse ocasionalmente interfira.', 15, 4);
+INSERT INTO tb_option (id, description, weight, question_id) 
+VALUES (1, 'Tenho equilíbrio total e tempo dedicado para lazer e autocuidado.', 25, 4);
 
 
 INSERT INTO tb_lead (name, email, phone, salesman_id, lead_status, lead_classification, total_score) 
