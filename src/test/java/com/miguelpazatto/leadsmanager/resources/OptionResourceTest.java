84971cb97dc86678ae197b5d3c850c431d28c666
Miguel Pazatto <miguelpazatto60@gmail.com>
2026-06-01 10:44:35 -0300

    test: option controller get requests

---
+++ src/test/java/com/miguelpazatto/leadsmanager/resources/OptionResourceTest.java
@@ -0,0 +1,137 @@
+package com.miguelpazatto.leadsmanager.resources;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miguelpazatto.leadsmanager.dto.OptionResponseDTO;
+import com.miguelpazatto.leadsmanager.entities.Option;
+import com.miguelpazatto.leadsmanager.entities.Question;
+import com.miguelpazatto.leadsmanager.infra.security.TokenService;
+import com.miguelpazatto.leadsmanager.repositories.UserRepository;
+import com.miguelpazatto.leadsmanager.services.OptionService;
+import com.miguelpazatto.leadsmanager.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OptionResource.class)
+@AutoConfigureMockMvc(addFilters = false)
+class OptionResourceTest {
+
+   @Autowired
+   private MockMvc mockMvc;
+
+   ObjectMapper objectMapper = new ObjectMapper();
+
+   @MockitoBean
+   private OptionService optionService;
+
+   @MockitoBean
+   private TokenService tokenService;
+
+   @MockitoBean
+   private UserRepository userRepository;
+
+    @Test
+    @DisplayName("Deve mostrar todas as Options quando lista não for vazia")
+    void findAll_WhenListIsNotEmpty_ReturnOK() throws Exception {
+        // given
+        OptionResponseDTO optionResponseDTO = new OptionResponseDTO(
+                1L,
+                "Enunciado da opção",
+                25
+        );
+        List<OptionResponseDTO> optionList = List.of(optionResponseDTO);
+
+        given(optionService.findAll()).willReturn(optionList);
+
+        // when - then
+        mockMvc.perform(get("/options")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].id").value(optionResponseDTO.id()));
+
+        verify(optionService, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma lista vazia quando não houver options")
+    void findAll_WhenListIsEmpty_ReturnOk() throws Exception {
+        // given
+        List<OptionResponseDTO> optionList = List.of();
+        given(optionService.findAll()).willReturn(optionList);
+
+        // when - then
+        mockMvc.perform(get("/options")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+
+        verify(optionService, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma Option quando houver ID correspondente")
+    void findById_WhenIdDoesExist_ReturnOk() throws Exception {
+        // given
+        OptionResponseDTO optionResponseDTO = new OptionResponseDTO(
+                1L,
+                "Enunciado da opção",
+                25
+        );
+        Long optionId = 1L;
+
+        given(optionService.findById(optionId)).willReturn(optionResponseDTO);
+
+        // when - then
+        mockMvc.perform(get("/options/{id}", optionId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(optionResponseDTO.id()))
+                .andExpect(jsonPath("$.description").value(optionResponseDTO.description()));
+
+        verify(optionService, times(1)).findById(optionId);
+    }
+
+    @Test
+    @DisplayName("Deve lançar ResourceNotFoundException quando o ID não existir")
+    void findById_WhenIdDoesNotExist_ReturnNotFound() throws Exception {
+        // given
+        Long optionId = 999L;
+        given(optionService.findById(optionId)).willThrow(new ResourceNotFoundException("Resource not found"));
+
+        // when - then
+        mockMvc.perform(get("/options/{id}", optionId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value("Resource not found"));
+
+        verify(optionService, times(1)).findById(optionId);
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
